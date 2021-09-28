a826e0cf647082d33d787a0f0fe786aae6d2c09c
Cunningham <cunsco@f01898096c90.ant.amazon.com>
2021-09-28 14:22:52 -0600

    Tue Sep 28 20:22:52 UTC 2021

--- pois-control.py
+++ pois-control.py
@@ -6,8 +6,21 @@ import math
 import os
 import xmltodict
 
+
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
+'''
+GET
+../channel/get = get all channels
+../channel/channel1 = get specific channel
+
+PUT
+../channel/create/channel1
+../channel/update/channel
+
+DELETE
+../channel/delete/channel1
+'''
 
 
 def lambda_handler(event, context):
